795970581d5446d4ec23b07ac0896704e82e15c6
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2022-09-28 15:50:58 +0900

    Next.jsでマイクロブログを構築（ページ遷移、スタイリング編）

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css"
+
+const name = "Shin Code";
+export const siteTtile = "Next.js blog"
+
+function Layout({children}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img className={utilStyles.borderCircle} src="/images/profile.png" />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
