b95e6c30af9061aa6acd3b12d9fc7ffc158e4211
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2022-10-04 13:33:57 +0900

    Next.jsでマイクロブログを構築（動的ルーティング編）

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
-import utilStyle from '../styles/utils.module.css'
+import Layout, { siteTtile } from '../components/Layout'
+import utilStyles from '../styles/utils.module.css'
 import { getPostsData } from '../lib/post'
 
 //SSGの場合
@@ -29,12 +29,15 @@ export async function getStaticProps() {
 // }
 
 export default function Home({allPostsData}) {
-  return <Layout>
-    <section>
-      <p className={utilStyle.headingMd}>私はフルスタックエンジニアです/Udemy講師として活動しています/好きな言語はJavascriptです</p>
+  return <Layout home>
+    <Head>
+      <title>{siteTtile}</title>
+    </Head>
+    <section className={utilStyles.headingMd}>
+      <p>私はフルスタックエンジニアです/Udemy講師として活動しています/好きな言語はJavascriptです</p>
     </section>
-    <section>
-      <h2>🗒エンジニアのブログ</h2>
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>🗒エンジニアのブログ</h2>
       <div className={styles.grid}>
         {allPostsData.map(({ id, title, date, thumbnail }) => (
           <article key={id}>
@@ -42,10 +45,10 @@ export default function Home({allPostsData}) {
               <img src={thumbnail} className={styles.thumbnailImage} />
             </Link>
             <Link href={`/posts/${id}`}>
-              <a className={utilStyle.boldText}>{title}</a>
+              <a className={utilStyles.boldText}>{title}</a>
             </Link>
             <br />
-            <small className={utilStyle.lightText}>{date}</small>
+            <small className={utilStyles.lightText}>{date}</small>
           </article>
         ))}
 
